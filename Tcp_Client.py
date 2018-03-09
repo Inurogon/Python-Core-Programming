5eb834866a5b3b6b9132182829638ad111573989
Inurogon <36781275+Inurogon@users.noreply.github.com>
2018-03-09 10:41:12 +0800

    Add files via upload

---
+++ Tcp_Client.py
@@ -0,0 +1,23 @@
+#tTCP_Socket Client
+from socket import *
+from time import ctime
+
+HOST='127.0.0.1' # or localhost
+PORT=2/1657
+BUFSIZ=1024
+ADDR=(HOST,PORT)
+
+
+tcpCliSock=socket(AF_INET,SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data=input('>')
+    if not data:
+        break
+    tcpCliSock.send(data)
+    data=tcpCliSock.recv(BUFSIZ)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+tcpCliSock.close()
